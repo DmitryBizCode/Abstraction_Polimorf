3f534791983804413b4f12ca20d07709e5df1582
Dmytro Beznosiuk <dmitrybeznosiukcode@gmail.com>
2023-10-30 23:49:24 +0200

    Добавьте файлы проекта.

---
+++ Settlement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_and_polimo
+{
+    internal class Settlement
+    {
+    }
+}
